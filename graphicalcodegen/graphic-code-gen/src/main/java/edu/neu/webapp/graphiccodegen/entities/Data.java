f0ce65f56f6c56e7716bb2d09ebeab89eb994e6d
Tej <tejaswini.hegde@gmail.com>
2013-05-19 23:00:34 -0400

    wrote entity class - Data

--- graphicalcodegen/graphic-code-gen/src/main/java/edu/neu/webapp/graphiccodegen/entities/Data.java
+++ graphicalcodegen/graphic-code-gen/src/main/java/edu/neu/webapp/graphiccodegen/entities/Data.java
@@ -1,5 +1,57 @@
 package edu.neu.webapp.graphiccodegen.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Data {
+	
+	@Id
+	private String dataName;
+	
+	private String dataValue;
+	
+	private String dataType;
+
+	public Data() {
+		super();
+	}
+
+	public Data(String dataName, String dataValue, String dataType) {
+		super();
+		this.dataName = dataName;
+		this.dataValue = dataValue;
+		this.dataType = dataType;
+	}
+
+	public String getDataName() {
+		return dataName;
+	}
+
+	public void setDataName(String dataName) {
+		this.dataName = dataName;
+	}
+
+	public String getDataValue() {
+		return dataValue;
+	}
+
+	public void setDataValue(String dataValue) {
+		this.dataValue = dataValue;
+	}
+
+	public String getDataType() {
+		return dataType;
+	}
+
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+
+	@Override
+	public String toString() {
+		return "Data [dataName=" + dataName + ", dataValue=" + dataValue
+				+ ", dataType=" + dataType + "]";
+	}
 
 }
