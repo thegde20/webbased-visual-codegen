7655d85ca8b48f58702d16c054913b5b531d9531
Janaki Ramachandran <janakihere@gmail.com>
2013-06-06 21:14:00 -0400

    Application Controller-added

---
+++ graphicalcodegen/graphic-code-gen/src/main/java/edu/neu/webapp/graphiccodegen/controller/ApplicationController.java
@@ -0,0 +1,86 @@
+package edu.neu.webapp.graphiccodegen.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.neu.webapp.graphiccodegen.dao.ApplicationDao;
+import edu.neu.webapp.graphiccodegen.dao.DeveloperDao;
+import edu.neu.webapp.graphiccodegen.dao.FlowDao;
+import edu.neu.webapp.graphiccodegen.model.Application;
+import edu.neu.webapp.graphiccodegen.model.Developer;
+import edu.neu.webapp.graphiccodegen.model.Flow;
+
+@Controller
+public class ApplicationController {
+	@Autowired
+	private ApplicationDao appDao;
+	@Autowired
+	private DeveloperDao devDao;
+	@Autowired
+	private FlowDao flowDao;
+
+	@RequestMapping(value = "/addApplication")
+	public String addApplication(ModelMap model, HttpServletRequest request) {
+		String name = request.getParameter("name");
+		String devEmail = request.getParameter("developer");
+		Developer devRequested = devDao.getDeveloper(devEmail);
+		
+		Application newapp = new Application(name, devRequested);
+		appDao.persist(newapp);
+		System.out.println("1.email---"+devRequested.getEmail());
+		devRequested.addApplication(newapp);
+		List<Application> allApps = appDao.getAllApplications();
+		model.put("apps", allApps);
+
+		List<Developer> allDevelopers = devDao.getAllDevelopers();
+		model.put("developers", allDevelopers);
+		
+		return "applicationList";
+
+	}
+
+	@RequestMapping(value = "/applicationList")
+	public String getAllData(ModelMap model, HttpServletRequest request) {
+
+		List<Application> allApps = appDao.getAllApplications();
+		model.put("apps", allApps);
+
+		List<Developer> allDevelopers = devDao.getAllDevelopers();
+		model.put("developers", allDevelopers);
+
+		return "applicationList";
+	}
+
+	@RequestMapping(value = "/editApp")
+	public String editDeveloper(ModelMap model, HttpServletRequest request) {
+
+		if (request.getParameter("deleteId") != null) {
+			String appId = request.getParameter("deleteId");
+			appDao.deleteById(appId);
+			List<Application> allApps = appDao.getAllApplications();
+			model.put("apps", allApps);
+			List<Developer> allDevelopers = devDao.getAllDevelopers();
+			model.put("developers", allDevelopers);
+
+			return "applicationList";
+		} else {
+			String appId = request.getParameter("detailId");
+			Application appRequested = appDao.getApplication(appId);
+			
+			List<Flow> allFlows = flowDao.getAllFlows();
+			model.put("flows", allFlows);
+
+			model.put("application", appRequested);
+			return "applicationDetails";
+		}
+
+	}
+
+
+}
